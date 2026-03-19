8c8d9eea2c61e63d1f689932709fe644eeb1e5c4
maruf-hasan7081 <marufhasanchowdhury06@gmail.com>
2026-03-19 15:40:38 +0600

    add

--- src/countries/Countries.jsx
+++ src/countries/Countries.jsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../country/Country';
 
 const Countries = ({ countryPromise }) => {
@@ -11,15 +11,36 @@ const countriesStyle = {
     gap: '15px'
     // test commit
 }
+const[Count , setCount] =useState(0)
+
+const handlevisited=()=>{
+
+  
+    
+    const newvisited=[...Count, Country]
+    setCount(newvisited)
+
+}
+
 
 return (
     <div>
-        <h1>Countries length is : {countries.length}</h1>
+        <h1>Total country : {countries.length}</h1>
+        <h2>Total country visited:{Count}</h2>
+        <ol>
+            {
+                countries.map((country, index) => <li key={index}>{country.name.common}</li>)
+            }
+        </ol>
 
         <div style={countriesStyle}>
             {
                 countries.map((country) => (
-                    <Country key={country.cca3} country={country}></Country>
+                    <Country key={country.cca3}
+
+                    handlevisited={handlevisited}
+
+                     country={country}></Country>
                 ))
             }
         </div>
--- src/country/Country.jsx
+++ src/country/Country.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './country.css'
 
-const Country = ({country}) => {
+const Country = ({country ,handlevisited}) => {
 
-    const countryStyle={
+
+    const[visited, setVisited] =useState(false)
+
+
+    const handleVisit =()=>{
+        // if(visited){
+        //     setVisited(false)
+        //      handlevisited()
+        // }
+        // else{
+        //     setVisited(true)
+        //      handlevisited()
+        // }
+
+        setVisited(visited?   false :true);
+        handlevisited(Country)
+       
+
         
-    border:'2px solid red',
-    borderRadius:'10px',
-    
-    padding: '10px',
-    
-
 
     }
 
+
+
+
     console.log(country.name.common);
     return (
-        <div style={countryStyle}>
+        <div className={`countryStyle ${visited ? 'color' : ''}`}>
             <img src={country.flags.flags.png} alt={country.name.common} />
             <h3>population:{country.population.population}</h3>
             
             <h3>Name :{country.name.common}</h3>
+
+            <p>Area: {country.area.area}</p>
+            <p>continents: {country.continents.continents}</p>
+            <p>capital:{country.capital.capital}</p>
+            <p>language:{country.languages.languages.ara}</p>
+
+
+
+            
+
+                 <button className={visited ? 'btn-color' :'butn-color'} onClick={handleVisit}>{visited? 'visited' : 'not visited'}</button>
+
+                
+            
+
+           
             
         </div>
     );
